39bbd01e0ae7fbe418f9327484f5c22f302d9f20
alexm <alexm@MSI>
2017-04-28 22:32:55 +0200

    ya pilla strings

--- P2_G2_AlexMichalec_MarcArdila/P2_G2_AlexMichalec_MarcArdila/main.cpp
+++ P2_G2_AlexMichalec_MarcArdila/P2_G2_AlexMichalec_MarcArdila/main.cpp
@@ -11,16 +11,19 @@ void main() {
 	std::unordered_map<std::string, std::string, std::string> mymap;
 	std::string line, result, elem1, elem2;
 	int pos1, pos2, pos3, end;
-
+	int res1, res2, res3;
 	while (getline(fentrada, line)) {
 		std::cout << line << std::endl;
 		pos1 = 0;
 		pos2 = line.find("=");
 		pos3 = line.find("+");
 		end = line.find("\n");
-		result = line.substr(pos1, pos2 - 1);
-		elem1 = line.substr(pos2 + 1, pos3 - 1);
-		elem2 = line.substr(pos3 + 1, end);
+		res1 = pos2 - pos1 - 1;
+		res2 = (pos3 - 1) - (pos2 + 1);
+		res3 = end - (pos3 + 1);
+		result = line.substr(pos1, res1);
+		elem1 = line.substr(pos2 + 2, res2);
+		elem2 = line.substr(pos3 + 2, res3);
 		//usar find para encontrar posicion y luego usarla
 		//calcular distancia entre una palabra y otra para que funcione el substr
 	};
