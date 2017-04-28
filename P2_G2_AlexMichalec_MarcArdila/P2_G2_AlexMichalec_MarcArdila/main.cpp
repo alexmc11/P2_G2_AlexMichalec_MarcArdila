95d8ccb1f307c97d714a57cce74052c174df142c
alexm <alexm@MSI>
2017-04-28 22:35:28 +0200

    coments

--- P2_G2_AlexMichalec_MarcArdila/P2_G2_AlexMichalec_MarcArdila/main.cpp
+++ P2_G2_AlexMichalec_MarcArdila/P2_G2_AlexMichalec_MarcArdila/main.cpp
@@ -13,18 +13,21 @@ void main() {
 	int pos1, pos2, pos3, end;
 	int res1, res2, res3;
 	while (getline(fentrada, line)) {
+
 		std::cout << line << std::endl;
+		//encontrar signos
 		pos1 = 0;
 		pos2 = line.find("=");
 		pos3 = line.find("+");
 		end = line.find("\n");
+		//operaciones para que cree el substr
 		res1 = pos2 - pos1 - 1;
 		res2 = (pos3 - 1) - (pos2 + 1);
 		res3 = end - (pos3 + 1);
+		//leer los elementos y almacenarlos en strings
 		result = line.substr(pos1, res1);
 		elem1 = line.substr(pos2 + 2, res2);
 		elem2 = line.substr(pos3 + 2, res3);
-		//usar find para encontrar posicion y luego usarla
-		//calcular distancia entre una palabra y otra para que funcione el substr
+		
 	};
 }
